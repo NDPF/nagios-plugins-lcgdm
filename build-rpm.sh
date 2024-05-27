efa59c59c9341c6305d7dc2f38d18bd6c98a4648
Andrew Pickford <anpickford@googlemail.com>
2024-05-27 11:26:17 +0200

    Fixups for python3

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION='0.9.7'
-RELEASE='1'
+RELEASE='2'
 
 tar --transform "s,^.,nagios-plugins-lcgdm-${VERSION}," -czvf ~/rpmbuild/SOURCES/nagios-plugins-lcgdm-${VERSION}.tar.gz .
 rpmbuild -bb --define "__version ${VERSION}" --define "__release ${RELEASE}" dist/nagios-plugins-lcgdm.spec
